a428f36af85869bca5cd256bbae2506bb638fce4
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2020-04-26 07:27:52 +0800

    Extends Encodable with jsonData function

---
+++ Sources/SGBUtilities/JSONData+Utilities.swift
@@ -0,0 +1,23 @@
+//
+//  JSONData+Utilities.swift
+//  
+//
+//  Created by Stuart Breckenridge on 26/04/2020.
+//
+
+import Foundation
+
+
+extension Encodable {
+    func jsonData() throws -> Data {
+        do {
+            let json = try JSONSerialization.jsonObject(with: JSONEncoder().encode(self), options: .allowFragments)
+            let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+            return data
+        } catch {
+            throw error
+        }
+    }
+}
+
+
--- Tests/SGBUtilitiesTests/SGBUtilitiesTests.swift
+++ Tests/SGBUtilitiesTests/SGBUtilitiesTests.swift
@@ -6,10 +6,9 @@ final class SGBUtilitiesTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        let deviceDescription = CurrentDevice().description()
+        let deviceDescription = CurrentDevice.default.description()
         print(deviceDescription)
         XCTAssert(deviceDescription != "")
-        
     }
 
     static var allTests = [
