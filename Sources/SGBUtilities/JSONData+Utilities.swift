0f61abbdd6c989356ef449c259f73a2c76810fa9
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2020-04-26 07:32:51 +0800

    public modifier.

--- Sources/SGBUtilities/JSONData+Utilities.swift
+++ Sources/SGBUtilities/JSONData+Utilities.swift
@@ -11,7 +11,7 @@ import Foundation
 extension Encodable {
     
     /// This function returns data from `Encodable` types that can be used in URL requests.
-    func jsonData() throws -> Data {
+    public func jsonData() throws -> Data {
         do {
             let json = try JSONSerialization.jsonObject(with: JSONEncoder().encode(self), options: .allowFragments)
             let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
