558dc9e0c6715c6aba3d57e3506466e60e81ec4b
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2020-05-02 07:18:50 +0800

    Adds UIView

---
+++ Sources/SGBUtilities/UIView+Utilities.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+Utilities.swift
+//  
+//
+//  Created by Stuart Breckenridge on 02/05/2020.
+//
+
+#if os(iOS)
+import UIKit
+
+
+extension UIView {
+    
+    
+    /// Convenience function to load a view from a `xib`. Example: `let myCustomView: CustomView = .fromNib()`
+    /// - Returns: `UIView`
+    class func fromNib<T: UIView>() -> T {
+        return Bundle(for: T.self).loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
+    }
+}
+
+
+
+#endif
