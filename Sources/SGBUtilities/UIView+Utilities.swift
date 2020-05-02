ac7358f14056273e1d88ca762c4ea61db8e7dd32
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2020-05-02 09:58:48 +0800

    public protection level.

--- Sources/SGBUtilities/UIView+Utilities.swift
+++ Sources/SGBUtilities/UIView+Utilities.swift
@@ -14,7 +14,7 @@ extension UIView {
     
     /// Convenience function to load a view from a `xib`. Example: `let myCustomView: CustomView = .fromNib()`
     /// - Returns: `UIView`
-    class func fromNib<T: UIView>() -> T {
+    public class func fromNib<T: UIView>() -> T {
         return Bundle(for: T.self).loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
     }
 }
