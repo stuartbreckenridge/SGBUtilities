15ad2ef1d3b4d2315068939150da3ed1de221536
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2020-05-05 17:54:44 +0800

    adds string utils

---
+++ Sources/SGBUtilities/String+Utilities.swift
@@ -0,0 +1,32 @@
+//
+//  String+Utilities.swift
+//  
+//
+//  Created by Stuart Breckenridge on 05/05/2020.
+//
+
+import Foundation
+
+extension String {
+
+    func extractedString(line: String) -> String? {
+        let pattern = "[0-9]{4}hrs"
+        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+        let matches = regex.matches(in: self, range: NSMakeRange(0, self.utf16.count))
+        let results = zip(matches, matches.dropFirst().map { Optional.some($0) } + [nil]).map { current, next -> String in
+            let range = current.range(at: 0)
+            let start = String.Index(utf16Offset: range.location, in: self)
+            let end = next.map { $0.range(at: 0) }.map { String.Index(utf16Offset: $0.location, in: self) } ?? String.Index(utf16Offset: self.utf16.count, in: self)
+            
+            return String(self.utf16[start..<end])!
+        }
+        
+        let res = dump(results)
+        let filter = res.filter({ $0.contains(line.uppercased()) })
+        if filter.count > 0 {
+            return filter[0]
+        }
+        return nil
+    }
+    
+}
